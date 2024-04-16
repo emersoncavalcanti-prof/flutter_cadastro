bfe5716774c665d395517bf82c38a2cc298d983f
Emerson Cavalcanti <emerson.cavalcanti@escolatecnicalimoeiro.com.br>
2024-04-16 19:30:49 -0300

    correcao nome da classe state

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,15 +1,15 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
-  State<HomePage> createState() => _HomePaGeState();
+  State<HomePage> createState() => _HomePageState();
 }
 
-class _HomePaGeState extends State<HomePage> {
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold();
   }
 }

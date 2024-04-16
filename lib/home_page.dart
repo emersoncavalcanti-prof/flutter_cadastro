d2abd84070a0f88bc430dd2c3f7f0ad3339daf4e
Emerson Cavalcanti <emerson.cavalcanti@escolatecnicalimoeiro.com.br>
2024-04-16 20:08:20 -0300

    add botao e edit

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -8,8 +8,42 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final _formKey = GlobalKey<FormState>();
+  final controllerEmail = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Cadastro'),
+        backgroundColor: Colors.blue,
+        foregroundColor: Colors.white,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.only(
+          top: 30,
+          left: 20,
+          right: 20,
+        ),
+        child: Form(
+          key: _formKey,
+          child: ListView(
+            children: [
+              TextFormField(
+                controller: controllerEmail,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Digite seu email',
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                child: const Text('Enviar'),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }

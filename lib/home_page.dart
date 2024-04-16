53697fe837b4d9691243a63f3540bbc697a812ea
Emerson Cavalcanti <emerson.cavalcanti@escolatecnicalimoeiro.com.br>
2024-04-16 19:28:01 -0300

    base do projeto

---
+++ lib/home_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/widgets.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePaGeState();
+}
+
+class _HomePaGeState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}

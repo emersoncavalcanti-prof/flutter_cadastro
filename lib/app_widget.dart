104af45b7916aba61c14a9bc98a8f84faa1a8c35
Emerson Cavalcanti <emerson.cavalcanti@escolatecnicalimoeiro.com.br>
2024-05-06 19:59:47 -0300

    add animacao com lottie

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_cadastro/anima_page.dart';
 import 'package:flutter_cadastro/detalhes_page.dart';
+import 'package:flutter_cadastro/entrega_page.dart';
 import 'package:flutter_cadastro/home_page.dart';
 
 class AppWidget extends StatelessWidget {
@@ -10,10 +11,11 @@ class AppWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      initialRoute: '/',
+      initialRoute: '/entrega',
       routes: {
         '/': (context) => const HomePage(),
         '/anima': (context) => const AnimaPage(),
+        '/entrega': (context) => const EntregaPage(),
         '/detalhes': (context) => const DetalhesPage(),
       },
     );
